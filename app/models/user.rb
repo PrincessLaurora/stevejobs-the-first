9dc12780ab9cdb3cf107eaa49eae5bbe5d5687f4
Laura Felce <crepu83@yahoo.it>
2019-09-02 09:30:52 +0000

    added trackable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable, :omniauthable
 
   has_many :listings
